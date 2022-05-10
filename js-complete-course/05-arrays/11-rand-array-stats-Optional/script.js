40bfb5fb3d8b24c7b95a499f2e077ee072587968
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-10 14:39:03 +0200

    ch5 ex11 done

--- js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
+++ js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
@@ -13,4 +13,21 @@
 
     // your code here
 
+
+    document.getElementById(`run`).addEventListener(`click`, () => {
+        const arr = [];
+        for(i = 0; i < 10; i++) {
+            arr.push(Math.floor(Math.random()*100))
+        }
+        const tableCells = document.getElementById(`rows`).children;
+        for(i = 0; i < arr.length; i++){
+            tableCells[i].innerHTML = arr [i];
+        }
+
+        document.getElementById('min').innerHTML = arr.reduce((prev,current) => Math.min(prev,current));
+        document.getElementById('max').innerHTML = arr.reduce((prev,current)=>Math.max(prev,current));
+        document.getElementById('sum').innerHTML = arr.reduce((prev,current)=>(prev+current));
+        document.getElementById('average').innerHTML = arr.reduce((prev,current)=>(prev+current))/arr.length;
+    });
+
 })();
