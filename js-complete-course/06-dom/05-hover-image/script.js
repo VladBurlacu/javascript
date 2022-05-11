f3048ccdd63f22803a682e8ed1cf04a65a07a4ed
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-11 09:26:34 +0200

    ch6 ex5 done

--- js-complete-course/06-dom/05-hover-image/script.js
+++ js-complete-course/06-dom/05-hover-image/script.js
@@ -13,4 +13,11 @@
 
     // your code here
 
+    let img = document.querySelector('img');
+    let start = img.src;
+    let hover = img.getAttribute('data-hover');
+
+    img.onmouseover = () => { img.src = hover; }
+    img.onmouseout = () => { img.src = start; }
+
 })();
