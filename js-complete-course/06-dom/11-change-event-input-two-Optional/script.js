5f36ed176537a7c19bea5c3bf2093f21bbad4202
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-12 10:02:55 +0200

    ch6 ex6 done, still don't understand regex

--- js-complete-course/06-dom/11-change-event-input-two-Optional/script.js
+++ js-complete-course/06-dom/11-change-event-input-two-Optional/script.js
@@ -12,5 +12,16 @@
 (function() {
 
     // your code here
+    document.getElementById(`pass-one`).minLength = 8;
+    document.getElementById(`pass-one`).addEventListener(`input`, () => {
+        let inputPass = document.getElementById(`pass-one`).value;
+        if (inputPass.length <8) {
+            document.getElementById(`validity`).innerHTML = `Not ok`;
 
+        } else if(!/(?:.*\d){2}/.test(inputPass)) {
+            document.getElementById('validity').innerHTML = 'Add 2 numbers!';
+        } else {
+            document.getElementById(`validity`).innerHTML = `ok`;
+        }
+    })
 })();
